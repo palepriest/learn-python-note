6f6c119c866ec065d78fc37f5e5bfa4affc3eb87
xue_kang <xue_kang@careri.com>
2021-01-29 11:05:13 +0800

    first commit.

---
+++ src/ex1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+print "hello world!"
+print "hello again!"
+print "just typing like this"
+print "isn't it fun?"
+print "it's really funny."
+print "ok, stop now."
+print "i said 'stop' now please."
+print 'well done.'
---
+++ src/ex2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# A comment is used to make code readable, 
+# anything after # will be ignored by python.
+
+print "I can write a comment like this." # comment after code
+
+# You can also use a comment to 'disable' a piece of code.
+# print 'code unused.'
+
+print 'code here wil run.'
